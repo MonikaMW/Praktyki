e553787b74fd3059c2503388d87666442833ad62
MonikaMW <151529352+MonikaMW@users.noreply.github.com>
2023-11-21 09:14:39 +0100

    Delete SQLQuery12.sql

--- SQLQuery12.sql
+++
@@ -1,9 +0,0 @@
-
-select EmployeeID, FirstName, LastName, ReportsTo,
-CASE 
-WHEN ReportsTo=2 THEN 'Andrew Fuller'
-WHEN ReportsTo=5 THEN 'Steven Buchanan'
-ELSE 'Prze³o¿ony'
-END as Prze³o¿eni
-from Employees;
-
